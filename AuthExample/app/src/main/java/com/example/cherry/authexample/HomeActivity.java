46d1f71e382895027becf5b5f3551cd9ad2ca307
chaitanya <chaitanyakarri963@gmail.com>
2020-08-26 09:47:26 +0530

    26-08-2020

--- AuthExample/app/src/main/java/com/example/cherry/authexample/HomeActivity.java
+++ AuthExample/app/src/main/java/com/example/cherry/authexample/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.cherry.authexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,5 +19,11 @@ public class HomeActivity extends AppCompatActivity {
         tv = findViewById(R.id.tv);
         auth = FirebaseAuth.getInstance();
         tv.setText("Welcome "+auth.getCurrentUser().getEmail());
+    }
+
+    public void signout(View view) {
+        auth.signOut();
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
--- AuthExample/app/src/main/java/com/example/cherry/authexample/MainActivity.java
+++ AuthExample/app/src/main/java/com/example/cherry/authexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.cherry.authexample;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,24 +18,24 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    EditText email, pass;
+    EditText mail, pass;
     FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email = findViewById(R.id.email);
+        mail = findViewById(R.id.email);
         pass = findViewById(R.id.pass);
         auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser()!=null){
-            startActivity(new Intent(this,HomeActivity.class));
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, HomeActivity.class));
             finish();
         }
     }
 
     public void login(View view) {
-        String umail = email.getText().toString();
+        String umail = mail.getText().toString();
         String upass = pass.getText().toString();
         if (umail.isEmpty() | upass.isEmpty()) {
             Toast.makeText(this, "Fill all the details",
@@ -42,13 +45,13 @@ public class MainActivity extends AppCompatActivity {
                     new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
+                            if (task.isSuccessful()) {
                                 Toast.makeText(MainActivity.this,
                                         "Successfully Logged in",
                                         Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                                startActivity(new Intent(MainActivity.this, HomeActivity.class));
                                 finish();
-                            }else{
+                            } else {
                                 Toast.makeText(MainActivity.this,
                                         "Failed to login", Toast.LENGTH_SHORT).show();
                             }
@@ -58,6 +61,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
-        startActivity(new Intent(this,RegisterActivity.class));
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View v = inflater.inflate(R.layout.reset, null);
+        builder.setView(v);
+        final EditText email = v.findViewById(R.id.umail);
+        builder.setCancelable(false);
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialogInterface, int i) {
+                String mail = email.getText().toString();
+                auth.sendPasswordResetEmail(mail).addOnCompleteListener(MainActivity.this,
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this,"Reset email sent",
+                                            Toast.LENGTH_SHORT).show();
+                                    dialogInterface.dismiss();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Failed",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 }
