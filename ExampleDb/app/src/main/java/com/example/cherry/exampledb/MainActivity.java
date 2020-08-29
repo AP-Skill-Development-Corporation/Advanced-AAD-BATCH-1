f87a5c92ab32b59b85c3f6a50a3792524673e940
chaitanya <chaitanyakarri963@gmail.com>
2020-08-29 09:18:33 +0530

    29-08-2020

--- ExampleDb/app/src/main/java/com/example/cherry/exampledb/MainActivity.java
+++ ExampleDb/app/src/main/java/com/example/cherry/exampledb/MainActivity.java
@@ -1,25 +1,57 @@
 package com.example.cherry.exampledb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.cherry.exampledb.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     DatabaseReference reference;
+    ArrayList<MyModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        list = new ArrayList<>();
         reference = FirebaseDatabase.getInstance().getReference("Data");
+        //Code to read the data from the firebase database
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    MyModel model = dataSnapshot.getValue(MyModel.class);
+                    list.add(model);
+                    MyAdapter myAdapter = new MyAdapter(MainActivity.this,list);
+                    binding.rv.setAdapter(myAdapter);
+                    binding.rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    binding.rv.addItemDecoration(
+                            new DividerItemDecoration(MainActivity.this,
+                                    DividerItemDecoration.VERTICAL));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "failed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -29,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         String snumber = binding.number.getText().toString();
         MyModel model = new MyModel(sname,sroll,snumber);
        //String id = reference.push().getKey();
+        //To insert the data into firebase database
         reference.child(sroll).setValue(model);
         Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
     }
--- ExampleDb/app/src/main/java/com/example/cherry/exampledb/MyAdapter.java
+++ ExampleDb/app/src/main/java/com/example/cherry/exampledb/MyAdapter.java
@@ -1,21 +1,33 @@
 package com.example.cherry.exampledb;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
     Context ct;
     ArrayList<MyModel> list;
-
+    DatabaseReference reference;
     public MyAdapter(Context ct, ArrayList<MyModel> list) {
         this.ct = ct;
         this.list = list;
@@ -29,10 +41,76 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HoldView> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HoldView holder, int position) {
+    public void onBindViewHolder(@NonNull HoldView holder, final int position) {
         holder.name.setText(list.get(position).getName());
         holder.roll.setText(list.get(position).getRoll());
         holder.number.setText(list.get(position).getNumber());
+        holder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference = FirebaseDatabase.getInstance().getReference("Data");
+                //Code to delete the data from the firebase database
+                reference.child(list.get(position).getRoll()).removeValue();
+                Toast.makeText(ct, "Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(ct);
+                ViewGroup group = view.findViewById(R.id.content);
+                View v = LayoutInflater.from(ct).inflate(R.layout.updata,
+                        group,false);
+                builder.setView(v);
+                builder.setCancelable(false);
+                final EditText sname = v.findViewById(R.id.name);
+                EditText sroll = v.findViewById(R.id.roll);
+                final EditText snum = v.findViewById(R.id.number);
+
+                sname.setText(list.get(position).getName());
+                sroll.setText(list.get(position).getRoll());
+                sroll.setEnabled(false);
+                snum.setText(list.get(position).getNumber());
+
+                builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        reference = FirebaseDatabase.getInstance().getReference();
+                        Query query = reference.child("Data").orderByChild("roll")
+                                .equalTo(list.get(position).getRoll());
+                        final HashMap<String,Object> map = new HashMap<>();
+                        query.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                                    map.put("name",sname.getText().toString());
+                                    map.put("number",snum.getText().toString());
+                                    dataSnapshot.getRef().updateChildren(map);
+                                    Toast.makeText(ct, "Data Updated",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+
+
+            }
+        });
+
     }
 
     @Override
